e29e8cd38cf7c88995631eef55b74c101140b659
zsakowitz <zsakowitz@gmail.com>
2021-08-08 09:01:05 -0700

    Remove nav icon

--- public/index.mjs
+++
@@ -1,8 +0,0 @@
-let icon = document.getElementsByTagName("nav-icon")[0];
-let nav = document.getElementsByTagName("nav")[0];
-
-icon.addEventListener("click", () => {
-  nav.classList.toggle("open");
-  
-  icon.classList.toggle("open", nav.classList.contains("open"));
-});
