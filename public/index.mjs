04592952e00984f17698a8f2b73d8114becd0767
zsakowitz <zsakowitz@gmail.com>
2021-08-09 14:04:00 -0400

    Add contextmenu handler to nav-icon

--- public/index.mjs
+++ public/index.mjs
@@ -1,5 +1,13 @@
-icon.addEventListener("click", async () => {
-  html.classList.toggle("nav");
-  
-  await $.local("nav", html.classList.contains("nav"));
-});
+let html = document.documentElement;
+let icon = document.getElementById("nav-icon");
+let nav = document.getElementById("nav");
+
+if (icon) {
+  icon.addEventListener("click", async () => {
+    html.classList.toggle("nav");
+    
+    await $.local("nav", html.classList.contains("nav"));
+  });
+
+  icon.addEventListener("contextmenu", (event) => event.preventDefault());
+}
