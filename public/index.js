4993fc9af586a03587bb2da73d257d9f15681767
zsakowitz <zsakowitz@gmail.com>
2021-08-09 09:38:44 -0400

    Add index.js as jquery addon

---
+++ public/index.js
@@ -0,0 +1,27 @@
+$.local = async function(key, value = undefined) {
+  if (value !== undefined) {
+    localStorage.setItem(key, JSON.stringify(value));
+    
+    window.dispatchEvent(new Event("storage", {key, value: newValue}));
+  } else {
+    let item = localStorage.getItem(key);
+    
+    try {
+      return JSON.parse(item);
+    } catch (error) {
+      return item;
+    }
+  }
+};
+
+$.onlocal = function(keyToTrack, cb) {
+  window.addEventListener("storage", ({key, newValue: value}) => {
+    if (key == keyToTrack) {
+      try {
+        cb(JSON.parse(value), key);
+      } catch {
+        cb(value, key);
+      }
+    }
+  });
+};
