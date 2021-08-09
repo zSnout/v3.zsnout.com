dc011872efa4774dd59d185efb03290d35bf9a50
zsakowitz <zsakowitz@gmail.com>
2021-08-09 11:15:00 -0400

    Move navigation bar into seperate folder

--- public/index.mjs
+++ public/index.mjs
@@ -1,27 +1,5 @@
-let field = document.getElementById("nav-input");
-let nav = document.getElementsByTagName("nav")[0];
-
-field.addEventListener("input", () => {
-  let words = field.value.toLowerCase().match(/[A-Za-z0-9]+/g);
-
-  if (!words || !words.length)
-    return [...document.querySelectorAll("nav a, nav-div, nav-name")]
-      .map(e => e.style.display = "block");
-
-  [...nav.getElementsByTagName("a")].map(e => {
-    let mywords = e.textContent.toLowerCase().match(/[A-Za-z0-9]+/g);
-
-    for (let word of words) {
-      if (mywords.indexOf(word) == -1) return e.style.display = "none";
-    }
-    
-    e.style.display = "block";
-  });
-
-  [...document.getElementsByTagName("nav-div")].map(e => {
-    if ([...e.children].every(e => e.style.display == "none")) e.style.display = "none";
-    else e.style.display = "block";
-
-    e.previousElementSibling.style.display = e.style.display;
-  });
+icon.addEventListener("click", async () => {
+  html.classList.toggle("nav");
+  
+  await $.local("nav", html.classList.contains("nav"));
 });
