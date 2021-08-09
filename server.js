87f5068934b0b4ff990051131c0df484fbf9a417
zsakowitz <zsakowitz@gmail.com>
2021-08-09 12:36:54 -0400

    Make <nav-icon> optional + add nav frame

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.decorate("load", async function (path) {
   require(__dirname + `/routes/${path}`)(app);
 });
 
-app.decorateReply("sendView", async function (view, data = {}) {
+app.decorateReply("sendView", async function (view, data = {}, navicon = true) {
   let layout = null;
   let title = "";
   let styles = ["/index.css"];
@@ -74,8 +74,8 @@ app.decorateReply("sendView", async function (view, data = {}) {
   for (let href of styles) resources.push(`<link rel="stylesheet" href="${escapeXML(href)}">`);
   for (let src of preload) resources.push(`<script src="${escapeXML(src)}"></script>`);
   for (let src of postload) resources.push(`<script src="${escapeXML(src)}" type="module"></script>`);
-
-  await this.view(`layout.ejs`, { body, title, resources, meta, escapeXML, indent });
+  
+  await this.view(`layout.ejs`, { body, title, resources, meta, navicon, escapeXML, indent });
 });
 
 app.load("index");
