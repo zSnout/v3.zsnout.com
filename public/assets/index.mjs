e760b6714883ee5fad42695d1a3fb7f1a5b9049f
zsakowitz <zsakowitz@gmail.com>
2021-08-10 08:59:40 -0400

    Use jquery convention of $ for variable names

--- public/assets/index.mjs
+++ public/assets/index.mjs
@@ -1,16 +1,14 @@
-let html = $(document.documentElement);
-let icon = $("#nav-icon");
-let nav = $("nav");
+let $html = $(document.documentElement);
+let $icon = $("#nav-icon");
+let $nav = $("nav");
 
-if (icon) {
-  icon.on("click", async () => {
-    html.toggleClass("nav");
-    
-    await $.local("nav", html.hasClass("nav"));
-  });
-}
+$icon.on("click", async () => {
+  $html.toggleClass("nav");
+  
+  await $.local("nav", $html.hasClass("nav"));
+});
 
-html.on("contextmenu", ({originalEvent: event}) => {
+$html.on("contextmenu", ({originalEvent: event}) => {
   event.path.map((element) => {
     if (element.hasAttribute && element.hasAttribute("nctx"))
       event.preventDefault();
