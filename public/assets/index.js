2ef25d1c83c777a61ffdfa444858925ea1634c9e
zsakowitz <zsakowitz@gmail.com>
2021-08-10 06:03:34 -0400

    Change vanilla handlers to jQuery handlers

--- public/assets/index.js
+++ public/assets/index.js
@@ -13,7 +13,7 @@ $.local = async function(key, value = undefined) {
   if (value !== undefined) {
     localStorage.setItem(key, JSON.stringify(value));
     
-    window.dispatchEvent(new Event("storage", {key, newValue: value}));
+    $(window).trigger("storage", {key, newValue: value});
   } else {
     let item = localStorage.getItem(key);
     
@@ -26,7 +26,7 @@ $.local = async function(key, value = undefined) {
 };
 
 $.onlocal = function(keyToTrack, cb) {
-  window.addEventListener("storage", ({key, newValue: value}) => {
+  $(window).on("storage", ({key, newValue: value}) => {
     if (key == keyToTrack) {
       try {
         cb(JSON.parse(value), key);
