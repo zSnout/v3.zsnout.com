cef92c978dff35edaef1adadedcdc4334c35285f
zsakowitz <zsakowitz@gmail.com>
2021-08-22 18:29:59 -0400

    Update terminal

--- server/terminal.mjs
+++ server/terminal.mjs
@@ -7,13 +7,14 @@ let commands = {
 let rl = createInterface({
   input: process.stdin,
   output: process.stdout,
+  historySize: 0,
 });
 
 rl.writeln = (data = "") => {
   rl.write(data + "\n");
 };
 
-rl.input = (query) => {
+rl.query = (query) => {
   return new Promise((resolve) => {
     rl.question(query, (resp) => {
       resolve(String(resp));
@@ -26,29 +27,34 @@ function locate(context, command) {
   let name = command.substr(0, idx);
   let rest = command.substr(idx).trimStart();
 
-  if (!context) return rl.writeln("ERROR: Command does not exist");
+  if (!context) return rl.writeln("ERROR: command does not exist");
 
   let cmd = context[command];
 
   if (idx == -1) {
     if (typeof cmd == "function") return cmd(rest);
-    else return rl.writeln("ERROR: Command does not exist");
+    else return rl.writeln("ERROR: command does not exist");
   } else {
     if (typeof cmd == "function") return cmd(rest);
     else if (typeof cmd == "object") return locate(cmd, rest);
-    else return rl.writeln("ERROR: Command does not exist");
+    else return rl.writeln("ERROR: command does not exist");
   }
 }
 
 async function callback(command) {
   await locate(commands, command);
 
-  rl.input("zSnout@node:~$ ").then(callback);
+  rl.query("zSnout@node:~$ ").then(callback);
 }
 
 export default function (app) {
   rl.app = app;
 
   rl.writeln();
-  rl.input("zSnout@node:~$ ").then(callback);
+  rl.query("zSnout@node:~$ ").then(callback);
+
+  rl.on("close", () => {
+    console.clear();
+    process.exit(0);
+  });
 }
