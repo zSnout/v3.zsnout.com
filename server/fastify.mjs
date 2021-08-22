b2f8ff1edb3bdb9a1c1481273a31d7cc03ba65f6
zsakowitz <zsakowitz@gmail.com>
2021-08-22 04:19:57 -0400

    Add fastify-static

--- server/fastify.mjs
+++ server/fastify.mjs
@@ -3,6 +3,7 @@ import ajvFormats from "ajv-formats";
 import decorators from "./decorators.mjs";
 import ejs from "ejs";
 import fastify from "fastify";
+import fastifyStatic from "fastify-static";
 import pointOfView from "point-of-view";
 import prepareClient from "./prepare-client.mjs";
 console.debug("server", "Loaded dependencies");
@@ -14,6 +15,12 @@ console.debug("ajv", "Started AJV");
 let app = fastify();
 app.schemaCompiler = (schema) => ajv.compile(schema);
 console.debug("fastify", "Started fastify");
+
+app.register(fastifyStatic, {
+  serve: false,
+  root: process.env.ROOT,
+});
+console.debug("fastify", "Added Reply.sendFile");
 
 app.register(pointOfView, {
   engine: { ejs },
