ea8d2c127c298a901e161ea70f55f49be55d2f86
zsakowitz <zsakowitz@gmail.com>
2021-08-23 11:28:24 -0400

    Remove empty space within navigation

--- routes/404.mjs
+++ routes/404.mjs
@@ -2,4 +2,8 @@ export default function (app) {
   app.setNotFoundHandler((req, res) => {
     res.sendView("home/404");
   });
+
+  app.setErrorHandler((err, req, res) => {
+    res.send({ message: err.message });
+  });
 }
--- routes/nav.mjs
+++ routes/nav.mjs
@@ -11,6 +11,10 @@ export default function (app) {
           Main: {
             Home: "/",
           },
+          Account: {
+            "Log In": "/account/log-in",
+            "Sign Up": "/account/sign-up",
+          },
         },
       },
       { frame: true }
