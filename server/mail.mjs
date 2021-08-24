891b3826baeec2e50c1e9f6500ee0398ebf3d889
zsakowitz <zsakowitz@gmail.com>
2021-08-24 05:00:30 -0400

    Remove test()

--- server/mail.mjs
+++ server/mail.mjs
@@ -13,8 +13,6 @@ let mailer = createTransport({
 });
 
 function send({ to, subject, text, html }) {
-  if (process.env.MODE == "DEV") return test({ to, subject, text });
-
   return mailer.sendMail({
     to,
     subject,
@@ -24,16 +22,6 @@ function send({ to, subject, text, html }) {
     inReplyTo: uuid.v4(),
     messageId: uuid.v4(),
   });
-}
-
-function test({ to, subject, text }) {
-  console.log(`
-EMAIL
-=====
-To: ${to}
-Subject: ${subject}
-${text}
-`);
 }
 
 console.debug("mail", "Initialized mailserver...");
