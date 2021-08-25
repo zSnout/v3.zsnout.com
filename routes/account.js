d60b8269853ab976d60182b85f88dda461c3397a
zsakowitz <zsakowitz@gmail.com>
2021-08-25 06:47:16 -0700

    Add descs, format document

--- routes/account.js
+++ routes/account.js
@@ -1,4 +1,6 @@
 export default function (app) {
+  app.static("account/sign-up.js", "account/sign-up/index.js");
+
   app.redirect("/account/sign-up");
   app.get("/account/sign-up/", (req, res) => res.sendView("account/sign-up"));
 }
