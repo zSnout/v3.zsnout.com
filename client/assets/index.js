111f75fd00c30b21458eae875ce9718d657571ec
zsakowitz <zsakowitz@gmail.com>
2021-08-26 09:21:49 -0700

    Add dialogs

--- client/assets/index.js
+++ client/assets/index.js
@@ -1,10 +1,4 @@
 let $html = $("html");
-let $icon = $("#nav-icon");
-let $nav = $("nav");
-
-$icon.on("click", async () => {
-  $html.toggleClass("nav");
-});
 
 $html.on("contextmenu", (event) => {
   let prevent = false;
@@ -17,3 +11,116 @@ $html.on("contextmenu", (event) => {
 
   if (prevent) event.preventDefault();
 });
+
+let $icon = $("#nav-icon");
+$icon.on("click", async () => {
+  $html.toggleClass("nav");
+});
+
+let $bg = $("#modal-bg");
+let $modal = $("#modal");
+let $info = $("#modal-info");
+let $field = $("#modal-input");
+let $buttons = $("#modal-buttons");
+
+let $ok = $("<button>");
+let $cancel = $("<button>");
+
+$.alert = function (message) {
+  return new Promise(async (resolve) => {
+    if ($html.hasClass("modal")) {
+      $html.removeClass("modal");
+      await Promise.wait(500);
+    }
+
+    $info.text(message);
+    $field.css("display", "none");
+
+    $ok.text("OK")[0].onclick = () => {
+      $html.removeClass("modal");
+      resolve();
+    };
+
+    $modal[0].onclick = (e) => e.stopPropagation();
+    $bg[0].onclick = () => {
+      $html.removeClass("modal");
+      resolve();
+    };
+
+    $buttons.empty();
+    $buttons.append($ok);
+
+    $html.addClass("modal");
+    $ok.focus();
+  });
+};
+
+$.confirm = function (message, ok = "OK", cancel = "Cancel") {
+  return new Promise(async (resolve) => {
+    if ($html.hasClass("modal")) {
+      $html.removeClass("modal");
+      await Promise.wait(500);
+    }
+
+    $info.text(message);
+    $field.css("display", "none");
+
+    $ok.text(ok)[0].onclick = () => {
+      $html.removeClass("modal");
+      resolve(true);
+    };
+
+    $cancel.text(cancel)[0].onclick = () => {
+      $html.removeClass("modal");
+      resolve(false);
+    };
+
+    $modal[0].onclick = (e) => e.stopPropagation();
+    $bg[0].onclick = () => {
+      $html.removeClass("modal");
+      resolve(false);
+    };
+
+    $buttons.empty();
+    $buttons.append($cancel, $ok);
+
+    $html.addClass("modal");
+    $cancel.focus();
+  });
+};
+
+$.prompt = function (message, initial = "", blank = false, ok = "Submit") {
+  return new Promise(async (resolve) => {
+    if ($html.hasClass("modal")) {
+      $html.removeClass("modal");
+      await Promise.wait(500);
+    }
+
+    $info.text(message);
+    $field.css("display", "block");
+
+    if (typeof blank == "string") [ok, blank] = [blank, false];
+
+    $ok.text(ok)[0].onclick = () => {
+      if (blank || $field.val()) {
+        $html.removeClass("modal");
+        resolve($field.val());
+      }
+    };
+
+    $modal[0].onclick = (e) => e.stopPropagation();
+    $bg[0].onclick = () => {
+      if (blank) {
+        $html.removeClass("modal");
+        resolve(null);
+      }
+    };
+
+    $buttons.empty();
+    $buttons.append($ok);
+
+    $html.addClass("modal");
+    await Promise.wait(250);
+    $field.focus();
+  });
+};
