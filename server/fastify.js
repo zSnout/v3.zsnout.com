5dbee81b203038fad037082f415702db44442201
zsakowitz <zsakowitz@gmail.com>
2021-08-29 08:02:43 -0700

    Merge prepare-client into fastify

--- server/fastify.js
+++ server/fastify.js
@@ -144,6 +144,21 @@ async function load(path) {
   console.debug("server", "Loaded route " + path);
 }
 
-import("./prepare-client.js");
+setImmediate(async () => {
+  async function load(path) {
+    await import(`${process.env.ROOT}/${path}.js`);
+  }
+
+  await load("routes/index");
+  await load("routes/404");
+  await load("routes/assets");
+  await load("routes/nav");
+  await load("routes/account");
+
+  app.listen(3000, "127.0.0.1");
+  console.debug("fastify", "Server started");
+
+  (await import("./terminal.js")).default(app);
+});
 
 export default app;
