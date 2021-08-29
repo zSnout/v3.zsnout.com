699d53c4dcdc41e01fe6ea855d143daab842e0a5
zsakowitz <zsakowitz@gmail.com>
2021-08-29 07:50:16 -0700

    Merge dbclasses into database

--- server/database.js
+++ server/database.js
@@ -1,5 +1,9 @@
 import fs from "node:fs";
 import * as uuid from "uuid";
+import { send } from "./mail.js";
+
+let db = { select, insert, update, remove, has };
+console.debug("database", "Started database");
 
 function protoless(k, v) {
   if (v && typeof v === "object" && !Array.isArray(v)) {
@@ -150,5 +154,55 @@ async function has(tablename, row, item) {
   }
 }
 
-console.debug("database", "Started database");
-export default { select, insert, update, remove, has };
+class PendingUser {
+  static async create(email, username) {
+    let emailCode = uuid.v4();
+    let displayName = username;
+    username = username.toLowerCase();
+
+    if (!app.verifyEmail(email) || !app.verifyUsername(username)) return false;
+
+    if (
+      (await db.has("pending_users", "username", username)) ||
+      (await db.has("pending_users", "email", email)) ||
+      (await db.has("users", "username", username)) ||
+      (await db.has("users", "email", email))
+    )
+      return false;
+
+    await db.insert("pending_users", {
+      email,
+      username,
+      display_name: displayName,
+      email_code: emailCode,
+    });
+
+    await send({
+      to: email,
+      subject: "Verify your account on zSnout!",
+      text: `Welcome to zSnout! Click this link to verify your account: ${process.env.HOST}/account/verify/${emailCode}`,
+    });
+
+    return true;
+  }
+
+  static async verify(code, password) {
+    let user = await db.select("pending_users", code, "email_code");
+    if (!user) return false;
+
+    await db.remove("pending_users", user.id);
+    await db.insert("users", {
+      username: user.username,
+      email: user.email,
+      display_name: user.display_name,
+      password: await app.hash(password),
+      session: uuid.v4(),
+    });
+
+    return true;
+  }
+}
+
+class User {}
+
+export { select, insert, update, remove, has, PendingUser, User };
