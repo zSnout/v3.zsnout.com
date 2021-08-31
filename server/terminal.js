879629ada7c3a150eea374273e851f43686dbc2f
zsakowitz <zsakowitz@gmail.com>
2021-08-31 14:24:43 -0700

    Update terminal

--- server/terminal.js
+++ server/terminal.js
@@ -1,9 +1,27 @@
+import evaluate from "./eval.js";
 import { createInterface } from "node:readline";
+
+let commands = {
+  __proto__: null,
+  clear() {
+    console.clear();
+    console.log("\x1b[2m\x1b[3mconsole was cleared\x1b[0m");
+  },
+  async exec(text) {
+    await evaluate(text);
+  },
+  async eval(text) {
+    console.log(await evaluate(text));
+  },
+  async notfound() {
+    throw new ReferenceError("command not found");
+  },
+};
 
 let rl = createInterface({
   input: process.stdin,
   output: process.stdout,
-  historySize: 0,
+  historySize: 20,
 });
 
 rl.writeln = (data = "") => {
@@ -18,21 +36,30 @@ rl.query = (query) => {
   });
 };
 
-async function evaluate(text) {
+async function runCommand(command) {
+  if (command[0] != "#" && commands[0] != ".") command = "#eval " + command;
+  command = command.substr(1);
+  command = command.split(" ");
+
+  let context = commands;
+  while (typeof (context = context?.[command.shift()]) == "object");
+
+  if (typeof context != "function") context = commands.notfound;
+
   try {
-    console.log(await eval(text));
+    await context(command.join(" "));
   } catch (error) {
     console.log(`\x1b[1;31mERROR: ${error?.message || error}\x1b[0m`);
   }
 
-  rl.query("\n$ ").then(evaluate);
+  rl.query("\n> ").then(runCommand);
 }
 
 export default function (app) {
-  rl.app = app;
+  global.app = app;
 
   rl.writeln();
-  rl.query("$ ").then(evaluate);
+  rl.query("> ").then(runCommand);
 
   rl.on("close", () => {
     console.clear();
