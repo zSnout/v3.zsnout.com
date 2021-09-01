0ba17db4b711e4792a864b5163aa3db5a679b5db
zsakowitz <zsakowitz@gmail.com>
2021-08-31 18:44:11 -0700

    Change nav route

--- routes/nav.js
+++ routes/nav.js
@@ -5,19 +5,16 @@ app.static("nav/index.js");
 
 app.redirect("/nav");
 app.get("/nav/", (req, res) => {
-  res.view(
-    "nav/index",
-    {
-      groups: {
-        Main: {
-          Home: "/",
-        },
-        Account: {
-          "Log In": "/account/log-in/",
-          "Sign Up": "/account/sign-up/",
-        },
+  res.view("nav/index", {
+    frame: true,
+    groups: {
+      Main: {
+        Home: "/",
+      },
+      Account: {
+        "Log In": "/account/log-in/",
+        "Sign Up": "/account/sign-up/",
       },
     },
-    { frame: true }
-  );
+  });
 });
