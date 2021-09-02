b0a7f5ad5bee9b396bc40562eb7c8d33bbd4a8a2
zsakowitz <zsakowitz@zsnout.com>
2021-09-02 09:18:33 -0700

    Empty directory

--- server/bcrypt.js
+++
@@ -1,9 +0,0 @@
-import bcrypt from "bcryptjs";
-
-export function hash(text) {
-  return bcrypt.hash(text, 10);
-}
-
-export function check(text) {
-  return bcrypt.compare(text, hash);
-}
--- server/eval.cjs
+++
@@ -1,5 +0,0 @@
-module.exports = function (text) {
-  return eval(text);
-};
-
-module.exports.default = module.exports;
