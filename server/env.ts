e002c87517029ca14a19b034cd21f5b24a6eea0b
zsakowitz <zsakowitz@zsnout.com>
2021-09-03 11:29:30 -0700

    Create env.ts

---
+++ server/env.ts
@@ -0,0 +1,9 @@
+import { existsSync, copyFileSync, readFileSync, writeFile } from "node:fs";
+import { config } from "dotenv";
+
+if (!existsSync(".env")) {
+  copyFileSync(".env-template", ".env");
+}
+
+config();
+console.log(process.env);
--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,4 @@
+import "./env.js";
 import "./app.js";
 import "./ajv.js";
 import "./bcrypt.js";
