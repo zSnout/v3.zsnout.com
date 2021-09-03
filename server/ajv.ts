4a11884bcc1f57c97004663366a7afe357f6f183
zsakowitz <zsakowitz@zsnout.com>
2021-09-02 17:56:09 -0700

    Update ajv.ts

--- server/ajv.ts
+++ server/ajv.ts
@@ -1,16 +1,9 @@
-import Ajv, { Format, SchemaObject } from "ajv";
-
-let formats: { [x: string]: Format } = {
+/** A list of formats that can be used for JSON schemas. */
+let formats = {
   username: /^[A-Za-z_][A-Za-z0-9_]{4,15}$/,
   password: /[A-Za-z].*[0-9]|[0-9].*[A-Za-z]/,
   email:
     /^[a-zA-Z0-9.!#$%&â€™*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
 };
 
-let ajv = new Ajv({ formats });
-
-function compile(schema: SchemaObject) {
-  return ajv.compile(schema);
-}
-
-export { compile, formats, ajv };
+export { formats };
