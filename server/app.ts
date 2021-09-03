be090124ebecf64e5a238226da54022ef38fd037
zsakowitz <zsakowitz@zsnout.com>
2021-09-02 18:53:55 -0700

    Add JSDoc comments

--- server/app.ts
+++ server/app.ts
@@ -1,8 +1,10 @@
 import fastify from "fastify";
 import { formats } from "./ajv.js";
 
-export default fastify({
+/** The main Fastify app. */
+let app = fastify({
   ajv: {
     customOptions: { formats },
   },
 });
+export default app;
