380fc6c05fe2dd6f5cdd40dc7e962c6a75a9247e
zsakowitz <zsakowitz@zsnout.com>
2021-09-03 11:03:36 -0700

    Start database.ts

---
+++ server/database.ts
@@ -0,0 +1,18 @@
+import { readFileSync, writeFile } from "node:fs";
+
+let db: {
+  tables: { [id: string]: object };
+  meta_tables: {
+    [tablename: string]: { [colname: string]: { [item: string]: string } };
+  };
+} = JSON.parse(readFileSync("../database.json", { encoding: "utf-8" }));
+
+function save() {
+  writeFile("../database.json", JSON.stringify(db), () => {
+    setTimeout(save, 15000);
+  });
+}
+
+setTimeout(save, 15000);
+
+let database = {};
