237ad660ab2e459d6ae0eccba724d98da69f43b7
zsakowitz <zsakowitz@zsnout.com>
2021-09-07 07:37:40 -0700

    Update scripts

--- server/build.ts
+++ server/build.ts
@@ -4,18 +4,10 @@ import { renderView } from "./ejs.js";
 
 glob("client/**/*.ejs", (err, files) => {
   for (let file of files) {
-    let now = Date.now();
     let name = file.substr("client/".length).replaceAll(".ejs", "");
 
     renderView(name).then((data) => {
-      console.log(`${file} ${Date.now() - now}ms`);
-
       writeFile(file.replaceAll(".ejs", ".html"), data, () => {});
     });
   }
-});
-
-let now = Date.now();
-process.on("beforeExit", () => {
-  console.log("completed in " + (Date.now() - now) + "ms");
 });
