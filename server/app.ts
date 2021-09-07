a58818825c7ce120d2b4eb5261ab7aaba2e8ee52
zsakowitz <zsakowitz@zsnout.com>
2021-09-06 18:35:34 -0700

    Register fastify-static

--- server/app.ts
+++ server/app.ts
@@ -1,6 +1,9 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 import fastify from "fastify";
 import fastifyStatic from "fastify-static";
 import { formats } from "./ajv.js";
+import { ServerResponse } from "http";
 
 /** The main Fastify app. */
 let app = fastify({
@@ -9,9 +12,15 @@ let app = fastify({
   },
 });
 
+/** Register fastify-static. */
 app.register(fastifyStatic, {
-  root: "/",
-  serve: false,
+  root: dirname(fileURLToPath(import.meta.url)) + "/../client/",
+  setHeaders(res: ServerResponse, path: string) {
+    if (path.substring(path.length - 3) == ".ts")
+      res.setHeader("content-type", "text/typescript");
+    else if (path.substring(path.length - 4) == ".ejs")
+      res.setHeader("content-type", "text/html");
+  },
 });
 
 export default app;
