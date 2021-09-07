bac96e3ecdd16f44168120a6d9b0237d50887985
zsakowitz <zsakowitz@zsnout.com>
2021-09-07 06:22:56 -0700

    Change 404 & error handlers

--- routes/404.ts
+++ routes/404.ts
@@ -1,7 +1,10 @@
 import app from "../server/app.js";
 
-app.setNotFoundHandler((req, res) => res.code(404).view("home/404"));
+app.setNotFoundHandler((req, res) => res.code(404).view("404"));
 
 app.setErrorHandler((err, req, res) => {
-  res.send({ error: true, message: String(err) });
+  res.send({
+    error: true,
+    message: err?.message,
+  });
 });
