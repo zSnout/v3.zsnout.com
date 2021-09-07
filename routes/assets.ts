3acf6727dc32b4669ef0cdef2d90620e27d53cf3
zsakowitz <zsakowitz@zsnout.com>
2021-09-07 06:58:25 -0700

    Update assets

--- routes/assets.ts
+++ routes/assets.ts
@@ -2,12 +2,16 @@ import { readFile } from "node:fs/promises";
 import app from "../server/app.js";
 
 app.get("/assets/react.js", (req, res) => {
+  res.header("content-type", "text/javascript");
+
   return readFile("./node_modules/react/umd/react.production.min.js", {
     encoding: "utf-8",
   });
 });
 
 app.get("/assets/react-dom.js", (req, res) => {
+  res.header("content-type", "text/javascript");
+
   return readFile("./node_modules/react-dom/umd/react-dom.production.min.js", {
     encoding: "utf-8",
   });
