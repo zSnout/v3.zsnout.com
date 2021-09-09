555334775c13ebf95085db1ae9e4b741bf7d6ca6
zsakowitz <zsakowitz@zsnout.com>
2021-09-09 06:00:36 -0700

    Make 404 page add trailing slash when missing

--- routes/404.ts
+++ routes/404.ts
@@ -1,6 +1,14 @@
 import app from "../server/app.js";
+import { extname } from "node:path";
 
-app.setNotFoundHandler((req, res) => res.code(404).view("404"));
+app.setNotFoundHandler((req, res) => {
+  let url = req.url;
+  let hasSlash = url.substr(url.length - 1) == "/";
+  let ext = extname(url);
+
+  if (!hasSlash && ext == "") res.redirect(req.url + "/");
+  else res.code(404).view("404");
+});
 
 app.setErrorHandler((err, req, res) => {
   res.send({
