44cf4d78400c0047a47f820b1dfa93aca3e8694b
zsakowitz <zsakowitz@zsnout.com>
2021-09-09 05:46:25 -0700

    Add contextmenu hider

--- client/assets/index.ts
+++ client/assets/index.ts
@@ -1,1 +1,20 @@
-$.root.on("contextmenu", ({ composedPath }) => {});
+$.root.on("contextmenu", (event) => {
+  let prevent = true;
+
+  for (let el of event.composedPath().reverse()) {
+    let elt = el as Element;
+    let eli = el as HTMLInputElement;
+
+    if (elt.hasAttribute?.("ctx")) prevent = false;
+    else if (elt.hasAttribute?.("nctx")) prevent = true;
+    else if (
+      elt.tagName == "INPUT" &&
+      eli.type != "submit" &&
+      eli.type != "button"
+    )
+      prevent = false;
+    else if (elt.tagName == "TEXTAREA") prevent = false;
+  }
+
+  if (prevent) event.preventDefault();
+});
